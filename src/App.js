a1ac7dddaff5b9364e2c9c713bcdd2f4be71f2e5
ParasSingla-009 <chutku202@gmail.com>
2024-07-29 16:16:39 +0530

    React App

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route
+              exact path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze"
+                  mode={mode}
+                />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase", "success")
+  };
+  const handleLoClick = () => {
+    console.log("Lowercase was clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleTiClick = () => {
+    console.log("Titlecase was clicked");
+    let newText = text.toLowerCase().replace(/(?:^|\s)\w/g, function(match) {
+        return match.toUpperCase();
+    });;
+    setText(newText);
+  };
+  const clearTextClick = () => {
+    console.log("ClearText was clicked");
+    let newText = ("");
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    console.log("Onchange was clicked");
+    setText(event.target.value);
+  };
+  const handleExtraSpaces = (event) => {
+    console.log("ExtraSpaces was Clicked");
+    setText(text.split(/\s+/).join(" "));
+
+  }
+
+  const wordCount = (text)=>{
+    let regex = /\s+\S+/;
+    let numOfWords = text.split(regex);
+    return numOfWords.length;
+  }
+  const [text, setText] = useState("Enter Text Here");
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          style={{ backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}}
+          id="myBox"
+          rows="8"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+        Convert to UpperCase
+      </button>
+      <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+        Convert to LowerCase
+      </button>
+      <button className="btn btn-primary mx-1" onClick={handleTiClick}>
+        Convert to TitleCase
+      </button>
+      <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+        Remove ExtraSpaces
+      </button>
+      <button className="btn btn-primary mx-1" onClick={clearTextClick}>
+        Clear Text
+      </button>
+    </div>
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h2>Your text Summary</h2>
+        {/* <p>{text.split(" ").length} words and {text.length} characters </p>
+        <p>{0.08 * text.split(" ").length} Minutes to Read</p> */}
+        <p>{text===""? 0 : wordCount(text)} words and {text.length} characters</p>
+        <p>{ text===""? 0 * 0.008 : wordCount(text) * 0.008} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text: "Enter your text to preview"}</p>
+
+    </div>
+    </>
+  );
+}
